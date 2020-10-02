0eef4544602c2bee6ff4f699da8dde965d4a08e4
Deni Gunawan <artdengun@gmail.com>
2020-10-02 20:29:11 +0700

    email

--- lib/Controller/EmailAuthentication.dart
+++ lib/Controller/EmailAuthentication.dart
@@ -1,8 +1,41 @@
-import 'package:flutter/material.dart';
+import 'package:desain_awal/Controller/GoogleAuthentication.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class EmailAuthentication extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+FirebaseAuth auth = FirebaseAuth.instance;
+
+Future<bool> signIn(String email, String passowrd) async {
+  try {
+    AuthResult result =
+        await auth.signInWithEmailAndPassword(email: email, password: email);
+    FirebaseUser user = result.user;
+    return Future.value(true);
+  } catch (e) {
+    switch (e.code) {
+      case 'ERROR_INVALID_EMAIL':
+        print('error');
+    }
   }
+}
+
+Future<bool> signUp(String email, String passowrd) async {
+  try {
+    AuthResult result = await auth.createUserWithEmailAndPassword(
+        email: email, password: email);
+    FirebaseUser user = result.user;
+    return Future.value(true);
+  } catch (e) {
+    switch (e.code) {
+      case 'ERROR_EMAIL_ALREADY_IN_USE':
+        print('error');
+    }
+  }
+}
+
+Future<bool> signOutUser() async {
+  FirebaseUser user = await auth.currentUser();
+
+  if (user.providerData[1].providerId == 'google.com') {
+    await googleSIgnIn.disconnect();
+  }
+  await auth.signOut();
 }
